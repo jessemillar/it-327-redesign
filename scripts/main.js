77429d36ecaff1bdddcbe74f833fc39d8bce996e
Jesse Millar <hellojessemillar@gmail.com>
2016-06-09 09:38:57 -0600

    Highlighting navigation

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,32 @@
 function init() {
     // Load in the page "templates" to the correct divs
-    $("#announcements-page").load("pages/announcements/index.html");
-    $("#labs-page").load("pages/labs/index.html");
     $("#additional-materials-page").load("pages/additional-materials/index.html");
-    $("#textbook-page").load("pages/textbook/index.html");
-    $("#lectures-page").load("pages/lectures/index.html");
+    $("#announcements-page").load("pages/announcements/index.html");
     $("#attendance-page").load("pages/attendance/index.html");
     $("#excellence-projects-page").load("pages/excellence-projects/index.html");
+    $("#labs-page").load("pages/labs/index.html");
+    $("#lectures-page").load("pages/lectures/index.html");
+    $("#textbook-page").load("pages/textbook/index.html");
 
     if (window.location.hash) { // If we're refreshing a page or opening a bookmark, open the proper tab
-        $(window.location.hash).addClass("active"); // Set the active tab
-
         var hashPage = window.location.hash.substring(1, window.location.hash.length);
 
-        bootpage.show(hashPage + "-page");
+        bootpage.show(hashPage + "-page", updateActiveNavigation);
     } else {
-        bootpage.show("announcements-page", updateActiveTab);
+        bootpage.show("announcements-page", updateActiveNavigation);
     }
 }
 
-function updateActiveTab() {
-    $("#device-control-tab").removeClass("active");
-    $("#display-tab").removeClass("active");
-    $("#audio-control-tab").removeClass("active");
-    $("#microphone-control-tab").removeClass("active");
+function updateActiveNavigation() {
+    $("#additional-materials-navigation").removeClass("active");
+    $("#announcements-navigation").removeClass("active");
+    $("#attendance-navigation").removeClass("active");
+    $("#excellence-projects-navigation").removeClass("active");
+    $("#labs-navigation").removeClass("active");
+    $("#lectures-navigation").removeClass("active");
+    $("#textbook-navigation").removeClass("active");
 
-    var currentTab = bootpage.currentPage.substring(0, bootpage.currentPage.length - 5);
+    var currentPage = bootpage.currentPage.substring(0, bootpage.currentPage.length - 5);
 
-    $("#" + currentTab + "-tab").addClass("active");
+    $("#" + currentPage + "-navigation").addClass("active");
 }
